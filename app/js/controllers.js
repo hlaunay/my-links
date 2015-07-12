f2fe79c0c4e6ccce99d029ea87a4d8135a117377
hlaunay <herve.launay@gmail.com>
2015-07-12 23:51:34 +0200

    Closed #3 - save preferences

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,24 +2,41 @@
 
 /* controllers */
 
-var myLinksControllers = angular.module('myLinksControllers', []);
+var myLinksControllers = angular.module('myLinksControllers', ['ngCookies']);
 
-myLinksControllers.controller('MyLinksCtrl', ['$scope', '$filter', 'Client', 'Environment', function($scope, $filter, Client, Environment) {
-	// init
-	$scope.clientName = '';
-	$scope.envName = '';
-	$scope.data = [];
+myLinksControllers.controller('MyLinksCtrl', ['$scope', '$filter', '$cookies', 'Client', 'Environment', 
+	function($scope, $filter, $cookies, Client, Environment) {
 
-	$scope.setEnvironment = function(client, name) {
-		$scope.clientName = client;
-		$scope.envName = name;
+		// init
+		$scope.clientName = '';
+		$scope.envName = '';
+		$scope.data = [];
 
-		$scope.data = Environment.query({envId: angular.lowercase(client).concat("-").concat(angular.lowercase(name))});
+
+		$scope.setEnvironment = function(client, name) {
+			$scope.clientName = client;
+			$scope.envName = name;
+
+			//set cookies
+			$cookies.put('clientName', client);
+			$cookies.put('envName', name);			
+
+			$scope.data = Environment.query({envId: angular.lowercase(client).concat("-").concat(angular.lowercase(name))});
+			$("#input_search").focus();
+		};
+
+		$scope.clients = Client.query(function(){
+			
+			// Try to get cookies
+			var cookieClientName = $cookies.get('clientName');
+			var cookieEnvName = $cookies.get('envName');
+
+			if(angular.isDefined(cookieClientName) && angular.isDefined(cookieEnvName)) {
+				$scope.setEnvironment(cookieClientName, cookieEnvName);
+			} else {
+				$scope.setEnvironment($scope.clients[0].name, $scope.clients[0].envs[0]);
+			}
+		});
 		$("#input_search").focus();
-	};
-
-	$scope.clients = Client.query(function(){
-		$scope.setEnvironment($scope.clients[0].name, $scope.clients[0].envs[0]);
-	});
-	$("#input_search").focus();
-}]);
+	}
+]);
